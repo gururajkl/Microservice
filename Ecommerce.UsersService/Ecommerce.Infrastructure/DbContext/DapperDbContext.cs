f0847a85de61b44201d4830dd01582c9d9fe8257
gururajkl <gururajragavendra7@gmail.com>
2026-02-14 19:55:15 +0530

    Create dapper db context

--- Ecommerce.UsersService/Ecommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ Ecommerce.UsersService/Ecommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -1,5 +1,20 @@
-﻿namespace Ecommerce.Infrastructure.DbContext;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace Ecommerce.Infrastructure.DbContext;
 
 public class DapperDbContext
 {
+    private readonly IDbConnection _dbConnection;
+
+    public DapperDbContext(IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("PostgresConnection");
+
+        // Create new Npgsql connection using the connection string from appsettings.
+        _dbConnection = new NpgsqlConnection(connectionString);
+    }
+
+    public IDbConnection DbConnection => _dbConnection;
 }
