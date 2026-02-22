9e2bfeab77ec4c9f3af6ed9c52ab7621d79373d0
gururajkl <gururajragavendra7@gmail.com>
2026-02-22 13:28:40 +0530

    Add FluentValidation for product add and update requests
    Introduce ProductAddRequestValidator and ProductUpdateRequestValidator to enforce validation rules on product add and update DTOs. These validators check for required fields, valid enum values, and appropriate value ranges, providing custom error messages for each rule.

---
+++ Ecommerce.ProductService/Ecommerce.ProductService.Core/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.ProductService.Core.DTOs;
+using FluentValidation;
+
+namespace Ecommerce.ProductService.Core.Validators;
+
+/// <summary>
+/// Create fluent validation for the <see cref="ProductAddRequestDto"/>.
+/// </summary>
+public class ProductAddRequestValidator : AbstractValidator<ProductAddRequestDto>
+{
+    public ProductAddRequestValidator()
+    {
+        RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product name cannot be empty");
+        RuleFor(p => p.CategoryOption).IsInEnum().WithMessage("Invalid category");
+        RuleFor(p => p.UnitPrice).InclusiveBetween(0, double.MaxValue)
+            .WithMessage($"Unit price should be between 0 and {double.MaxValue}");
+        RuleFor(p => p.Quantity).InclusiveBetween(0, int.MaxValue)
+            .WithMessage($"Unit price should be between 0 and {int.MaxValue}");
+    }
+}
---
+++ Ecommerce.ProductService/Ecommerce.ProductService.Core/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.ProductService.Core.DTOs;
+using FluentValidation;
+
+namespace Ecommerce.ProductService.Core.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequestDto>
+{
+    public ProductUpdateRequestValidator()
+    {
+        RuleFor(p => p.ProductId).NotEmpty().WithMessage("Product id cannot be empty");
+        RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product name cannot be empty");
+        RuleFor(p => p.CategoryOption).IsInEnum().WithMessage("Invalid category");
+        RuleFor(p => p.UnitPrice).InclusiveBetween(0, double.MaxValue)
+            .WithMessage($"Unit price should be between 0 and {double.MaxValue}");
+        RuleFor(p => p.Quantity).InclusiveBetween(0, int.MaxValue)
+            .WithMessage($"Unit price should be between 0 and {int.MaxValue}");
+    }
+}
